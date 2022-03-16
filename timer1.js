fa208a6e610cbac4dc5ccc59d4c8effe543824ec
linzhu <lin.lyn.zhu@gmail.com>
2022-03-15 17:48:45 -0700

    isNaN // change string to number // only execute number

---
+++ timer1.js
@@ -0,0 +1,18 @@
+const strs = process.argv.slice(2);
+let times = [];
+for (let str of strs) {
+  times.push(Number(str));
+}
+
+for (let time of times) {
+  if (!isNaN(time)) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, time * 1000);
+  } else {
+    break;
+  }
+}
+
+
+
