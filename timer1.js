cbae6dd409f65c9c7952c6640bd1fd7a3a243df7
linzhu <lin.lyn.zhu@gmail.com>
2022-03-15 22:17:04 -0700

    sort out the time number

--- timer1.js
+++ timer1.js
@@ -3,7 +3,9 @@ let times = [];
 for (let str of strs) {
   times.push(Number(str));
 }
-
+times.sort(function(a, b) {
+  return a - b;
+});
 for (let time of times) {
   if (!isNaN(time)) {
     setTimeout(() => {
